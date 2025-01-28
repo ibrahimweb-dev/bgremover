e529d80f015b6191e3006d3477b9b79d85aa358f
Ibrahim Abdi <147508042+ibrahimweb-dev@users.noreply.github.com>
2025-01-28 23:33:04 +0300

    Update userRoutes.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,7 +1,7 @@
 import express from "express";
-import handler, {clerkWebhooks} from '../controller/userController.js'
+import {clerkWebhooks} from '../controller/userController.js'
 const userRouter = express.Router();
 
 userRouter.post('/webhooks', clerkWebhooks);
-userRouter.post('hello',handler)
+
 export default userRouter
