3fd8d11d18bf151694a2d2618d66f7b4399bf7fa
Abdi <ibrahim095.com@gmail.com>
2025-01-29 21:47:54 +0300

    add

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,10 +1,7 @@
 import UserModel from "../models/userModels.js";
 import { Webhook } from "svix";
 
-
-
 const CreateWebhook = async (req, res) => {
-  
     try {
         const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
         await whook.verify(JSON.stringify(req.body), {
@@ -13,53 +10,60 @@ const CreateWebhook = async (req, res) => {
             'svix-signature': req.headers['svix-signature']
         });
 
-            const { data, type } = req.body
+        const { data, type } = req.body;
 
-
-            switch (type) {
-                case 'user.created':{
-                    const userData = {
-                        clerkId: data.id,
-                        email: data.email_addresses[0].email_address,
-                        firstname: data.first_name,
-                        lastname: data.last_name,
-                        photo: data.image_url
-                    }
-                    await UserModel.create(userData)
-                    res.json({message: 'User created'})
-
-                    break;
-                }
-                case 'user.updated':{
-                   const userData = {
-                       email: data.email_addresses[0].email_address,
-                       firstname: data.first_name,
-                       lastname: data.last_name,
-                       photo: data.image_url
-                   }
-                   await UserModel.updateOne({clerkId: data.id}, userData)
-                   res.json({message: 'User updated'})
-
-                    break;
-                }
-                case 'user.deleted':{
-                    await UserModel.deleteOne({clerkId: data.id})
-                    res.json({message: 'User deleted'}) 
-                    break;
-                }
-                default:
-                    break;
+        switch (type) {
+            case 'user.created': {
+                const userData = {
+                    clerkId: data.id,
+                    email: data.email_addresses[0].email_address,
+                    firstname: data.first_name,
+                    lastname: data.last_name,
+                    photo: data.image_url
+                };
+                await UserModel.create(userData);
+                return res.status(201).json({ message: 'User created' });
             }
-      
+
+            case 'user.updated': {
+                const userData = {
+                    email: data.email_addresses[0].email_address,
+                    firstname: data.first_name,
+                    lastname: data.last_name,
+                    photo: data.image_url
+                };
+
+                // Check if user exists and update
+                const updatedUser = await UserModel.findOneAndUpdate(
+                    { clerkId: data.id },
+                    userData,
+                    { new: true }
+                );
+
+                if (!updatedUser) {
+                    return res.status(404).json({ message: 'User not found for update' });
+                }
+
+                return res.json({ message: 'User updated' });
+            }
+
+            case 'user.deleted': {
+                const deletedUser = await UserModel.deleteOne({ clerkId: data.id });
+                if (deletedUser.deletedCount === 0) {
+                    return res.status(404).json({ message: 'User not found for deletion' });
+                }
+                return res.json({ message: 'User deleted' });
+            }
+
+            default: {
+                console.warn(`Unexpected event type: ${type}`);
+                return res.status(400).json({ message: 'Unhandled event type' });
+            }
+        }
     } catch (error) {
-        console.log(error)
-        res.json({message: error.message})
-        
+        console.error('Webhook error:', error);
+        return res.status(500).json({ message: 'Internal server error', error: error.message });
     }
+};
 
-}
-
-
-
-
-export {CreateWebhook}
+export { CreateWebhook };
