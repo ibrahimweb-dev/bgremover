c5ca6a890d535a0683f3d75b634ca9b04ffdb796
Abdi <ibrahim095.com@gmail.com>
2025-01-29 13:52:17 +0300

    added body-parser

--- server/index.js
+++ server/index.js
@@ -1,36 +1,29 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
+import bodyParser from 'body-parser'; // Import body-parser
 import connectDB from './config/db.js';
 import userRouter from './routes/userRoutes.js';
 
-
-//App config
+// App config
 const PORT = process.env.PORT || 5000;
 const app = express();
 connectDB();
-//inialize middleware
-app.use(express.json());
+
+// Middleware
+app.use(express.json()); // General JSON body parsing
 app.use(cors());
 
+// Use raw body parser for webhook route
+app.use('/api/user/webhooks', bodyParser.raw({ type: 'application/json' }));
 
-
-//Api routes
+// Routes
 app.get('/', (req, res) => {
     res.status(200).send('Hello World');
 });
-
-
-
 app.use('/api/user', userRouter);
 
-
-
-
-
-
-
-
+// Server
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
-}); 
+});
